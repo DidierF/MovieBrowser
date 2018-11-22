146518523f80fc821352416bef63bc0cea11b734
Didier Fuentes <didier.fuentes@gmail.com>
2018-11-22 19:20:20 -0400

    Initial Commit

---
+++ MovieBrowser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieBrowser
+//
+//  Created by Didier Fuentes on 11/22/18.
+//  Copyright © 2018 Didier Fuentes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
