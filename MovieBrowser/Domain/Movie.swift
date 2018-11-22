4f181fc21f436169caec0c2bf70d824b0d4eaec1
Didier Fuentes <didier.fuentes@gmail.com>
2018-11-22 19:50:51 -0400

    Added Movie class and core data entity.

---
+++ MovieBrowser/Domain/Movie.swift
@@ -0,0 +1,14 @@
+//
+//  Movie.swift
+//  MovieBrowser
+//
+//  Created by Didier Fuentes on 11/22/18.
+//  Copyright © 2018 Didier Fuentes. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class Movie: NSManagedObject {
+
+}
